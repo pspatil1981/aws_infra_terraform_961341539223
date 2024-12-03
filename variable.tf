c1fba59e90d15cf243377eeac2bc957b634ce448
pspatil1981 <40274174+pspatil1981@users.noreply.github.com>
2024-12-03 16:11:46 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,39 +1,49 @@
-#argument ke value ka datatype declare karte hai
-variable "aws_region" {
-  type = string
-}
+#argument ka value datatype declare karte hai 
+
+
 
 
 variable "instance_type" {
-  type = string
+  type    = string
+  default = "t2.small"
 }
 
 variable "instance_count" {
   type = number
 }
 
+variable "aws_region" {
+  description = "AWS region where resources will be created"
+  type        = string
+  default     = "us-east-1"
+}
 
 variable "vpc_cidr" {
   description = "CIDR block for the VPC"
+  type        = string
   default     = "10.0.0.0/16"
 }
 
-variable "public_subnets" {
+variable "public_subnet_cidrs" {
   description = "CIDR blocks for public subnets"
   type        = list(string)
   default     = ["10.0.1.0/24", "10.0.2.0/24"]
 }
 
-variable "private_subnets" {
+variable "private_subnet_cidrs" {
   description = "CIDR blocks for private subnets"
   type        = list(string)
   default     = ["10.0.3.0/24", "10.0.4.0/24"]
 }
 
 variable "availability_zones" {
-  description = "Availability zones for subnets"
+  description = "List of availability zones for subnets"
   type        = list(string)
-  default     = ["ap-south-1a", "ap-south-1b"]
+  default     = ["us-east-1a", "us-east-1b"]
 }
 
-
+variable "project_name" {
+  description = "Project name for tagging resources"
+  type        = string
+  default     = "MyVPCProject"
+}

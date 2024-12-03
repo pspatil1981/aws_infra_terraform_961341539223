3d12270d30893b56d79964036b486326afd2f4b0
pspatil1981 <pspatil1981@gmail.com>
2024-12-03 11:01:41 +0530

    adding vpc infra code

--- variable.tf
+++ variable.tf
@@ -12,3 +12,28 @@ variable "instance_count" {
   type = number
 }
 
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnets" {
+  description = "CIDR blocks for public subnets"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "private_subnets" {
+  description = "CIDR blocks for private subnets"
+  type        = list(string)
+  default     = ["10.0.3.0/24", "10.0.4.0/24"]
+}
+
+variable "availability_zones" {
+  description = "Availability zones for subnets"
+  type        = list(string)
+  default     = ["us-east-1a", "us-east-1b"]
+}
+
+
